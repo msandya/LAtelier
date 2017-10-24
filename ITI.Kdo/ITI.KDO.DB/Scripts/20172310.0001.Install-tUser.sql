98805da702336b551619c15e00faa159e361fda2
Unknown <trung.lespace@gmail.com>
2017-10-24 11:44:30 +0200

    [Architecture Project]
    _ITI.KDO.DAL added.
    _ITI.KDO.WebApp added.

---
+++ ITI.Kdo/ITI.KDO.DB/Scripts/20172310.0001.Install-tUser.sql
@@ -0,0 +1,17 @@
+﻿create table iti.tUser
+(
+	UserId int identity(0,1),
+	Pseudo nvarchar(150) not null,
+	FirstName nvarchar(150) not null,
+	LastName nvarchar(150) not null,
+	Email nvarchar(150) not null,
+	PhoneTel nvarchar(150)
+
+
+	constraint PK_tUser primary key(UserId),
+	constraint UK_tUser unique(Pseudo),
+	constraint CK_tUser_Pseudo check(Pseudo <> N'')
+);
+
+insert into iti.tUser(Pseudo, FirstName, LastName, Email) 
+values				 ('N'   , 'N'      , 'N'     , 'N');
---
+++ ITI.Kdo/ITI.KDO.DB/Scripts/20172310.0002.Install-vUser.sql
@@ -0,0 +1,11 @@
+﻿create view iti.vUser
+as
+	select
+		UserId = t.UserId,
+		Pseudo = t.Pseudo,
+		FirstName = t.FirstName,
+		LastName = t.LastName,
+		Email = t.Email,
+		PhoneTel = t.PhoneTel
+	from iti.tUser t
+	where t.UserId <> 0;
