898659c3030ea9bbd880268ad1b4ae88819b4574
msandya <madione@intechinfo.fr>
2017-11-15 10:44:48 +0100

    [GoogleConnection]Updated
    Updated tGoogleUser.sql,
    Updated sUserAddGoogleToken.sql

--- kdo/ITI.KDO.DB/Scripts/024.Install-sUserAddGoogleToken.sql
+++ kdo/ITI.KDO.DB/Scripts/024.Install-sUserAddGoogleToken.sql
@@ -1,12 +1,11 @@
 create procedure dbo.sUserAddGoogleToken
 (
     @UserId       int,
-    @GoogleId     varchar(32),
-    @RefreshToken varchar(64)
+	@RefreshToken varchar(64)
 )
 as
 begin
-    insert into dbo.tGoogleUser(UserId, GoogleId, RefreshToken)
-                         values(@UserId, @GoogleId, @RefreshToken);
+    insert into dbo.tGoogleUser(UserId, RefreshToken)
+                         values(@UserId, @RefreshToken);
     return 0;
 end;
